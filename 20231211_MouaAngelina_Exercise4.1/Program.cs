f15537fb115ad7c089f0648e3e2b82bac4b6a3a3
mouaa <mouaa@DESKTOP-NT2AJPK>
2023-12-11 13:38:16 -0500

    Add project files.

---
+++ 20231211_MouaAngelina_Exercise4.1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20231211_MouaAngelina_Exercise4._1
+{
+    internal class Perfect
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a number: ");
+            int userInput = Convert.ToInt16(Console.ReadLine());
+            int checkNumber = 0;
+            while (checkNumber == 0)
+            {
+                if (userInput == 6)
+                {
+                    Console.WriteLine("Perfect Number");
+                    Console.ReadLine();
+                    checkNumber = 1;
+                }
+                else if (userInput == 28)
+                {
+                    Console.WriteLine("Perfect Number");
+                    Console.ReadLine();
+                    checkNumber = 1;
+
+                }
+                else if (userInput == 496)
+                {
+                    Console.WriteLine("Perfect Number");
+                    Console.ReadLine();
+                    checkNumber = 1;
+
+                }
+                else if (userInput == 8128)
+                {
+                    Console.WriteLine("Perfect Number");
+                    Console.ReadLine();
+                    checkNumber = 1;
+
+                }
+                else if (checkNumber < 1)
+                {
+                    Console.WriteLine("Not a perfect number.");
+                    Console.ReadLine();
+                }
+            }
+        
+
+
+
+            
+        }
+    }
+}
+
